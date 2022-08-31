6c0719320aa123b65ada6f2b8ab73468b533210d
linjadon <38975269+linjadon@users.noreply.github.com>
2022-08-31 18:04:01 +0800

    Create spider_request_v1.py

--- spider_request_v1.py
+++ spider_request_v1.py
@@ -1,10 +1,22 @@
+#pip install beautifulsoup4
 import requests
 import re
 import os
 import time
 from bs4 import BeautifulSoup
 
+'''
+				NOTE
 
+#		print(soup.prettify())	#格式化输出
+#		one_info.h2.get_text()	#唯一项时可以精确获取字符
+#		one_info.a['href'])	#获取标签内的属性值
+#	<a class="" data-v-7f856186="" href="/detail/1">
+#	<img class="cover" data-v-7f856186="" src="https://p0.meituan.net/movie/ce4da3e03e655b5b88ed31b5cd7896cf62472.jpg@464w_644h_1e_1c"/>
+#	</a>
+#		soup.find_all('div','el-card item m-t is-hover-shadow')	llist format
+		sout.find('div','test') 	
+'''
 
 class spider_web:
 	def __init__(self,url,User_Agent,content_type,username,password):
@@ -25,10 +37,30 @@ class spider_web:
 #		将TXT格式的文件进行 LXML 格式转换， soup.prettfy()按照标准XML格式输出
 
 		soup = BeautifulSoup(page.text,'lxml')
-#		print(soup.prettify())
 
-		print (soup.div['id'])
-		print(soup.find_all(id='app'))
+
+#		print (soup.div['id'])
+		all_info = soup.find_all('div','el-card item m-t is-hover-shadow')
+		one_info = soup.find('div','el-card item m-t is-hover-shadow')
+
+		def has_span_not_class(self):
+			return  not one_info.has_attr('class')
+
+		print(one_info)
+		print(one_info.h2.get_text())
+		print(one_info.span.get_text())
+		print(str(one_info.img['src']))
+		print('*'*100)
+		print(one_info.find('div','categories').find_all('span'))
+		print(one_info.find('div','m-v-sm info').find_all('span'))
+		for i in all_info:
+#			print(i.find('class'.''))
+#			video_name = i.h2.get_texxt()
+#			video_link = i.a.get_text()
+#			pic_link = i.img['src']
+			print('*'*100)
+#		print(soup.find('div','el-card item m-t is-hover-shadow'))
+
 
 
 	def url_manager():
